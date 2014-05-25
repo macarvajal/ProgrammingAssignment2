5d64ae0349f0cfbb3c9f873f3b9f29d1007a19df
Migue <macarvajal@gmail.com>
2014-05-25 09:02:07 +0200

    Add comments and correct naming

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching computations. This source file contains two functions
+## used to compute and cache the inverse of a matrix. These functions
+## allows to reduce the execution time, by obtaining the inverse
+## of a matrix only once, and, later, retrieving the cached matrix 
+## inverse when it is needed.
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## Builds a named list of four functions to store 
+## a matrix and cache its inverse
+##
+## Args:
+##      x: The matrix that will be stored
+## 
+## Returns:
+##      A named list of functions to manage the matrix and its inverse
+##      get: retrieves the original matrix
+##      set: updates the original matrix
+##      setInverse: stores in the cache the inverse of the original matrix
+##      getInverse: retrieves from the cache the inverse of the original matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
+        ## x stores the matrix
+        ## m caches the inverse of x
+        m <- NULL 
+        
+        ## set
+        ## Stores a new original matrix
+        ## and resets the cache for inverse matrix
+        ##
+        ## Args:
+        ##       y: The new original matrix  
+        set <- function(y) {                
+                x <<- y 
                 m <<- NULL
         }
+        
+        ## get
+        ## Retrieves the original matrix
+        ## 
+        ## Returns:
+        ##       The original stored matrix
         get <- function() x
-        setinverse <- function(mean) m <<- mean
+        
+        ## setinverse
+        ## Stores in the cache the a new inverse matrix
+        ## It uses the <<- operator to access to the variable "m"
+        ## in the enclosing environment the variable m
+        ##
+        ## Args:
+        ##       inverse: The new inverse matrix to cache
+        setinverse <- function(inverse) m <<- inverse 
+        
+        ## getinverse
+        ## Retrieves the inverse of the matrix from cache
+        ##
+        ## Returns:
+        ##       The cached inverse matrix
         getinverse <- function() m
+        
+        ## Build a named list with the above functions
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
@@ -19,18 +65,34 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+## Computes a matrix inverse or retrieves its cached value
+## to speed-up program
+##
+## Args:
+##      x: A "cacheMatrix" built with makeCacheMatrix
+##
+## Returns:
+##      The inverse of the matrix stored in x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        m <- x$getinverse()
+        ## try to obtain the cached inverse
+        m <- x$getinverse()     
         if(!is.null(m)) {
+                ## if cached inverse matrix exists, we are done. Return it
                 message("getting cached data")
-                return(m)
+                return(m)       
         }
+        
+        ## If there is no inverse matrix cached, compute it. 
+        ## Obtain the original matrix from the cacheMatrix
         data <- x$get()
+        ## Compute the inverse of the original matrix
         m <- solve(data, ...)
+        ## Store the inverse of the original matrix in the cache
         x$setinverse(m)
+        
+        ## Return the inverse from the original matrix computed just before 
         m
 
 }
